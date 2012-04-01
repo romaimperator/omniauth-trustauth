e4ac9bbf4061b1a08aa2d0f0fb1faf551daea352
romaimperator <romaimperator@gmail.com>
2012-04-01 15:53:05 -0500

    Initial commit

---
+++ lib/omniauth-trustauth.rb
@@ -0,0 +1,7 @@
+require "omniauth-trustauth/version"
+
+module Omniauth
+  module Trustauth
+    # Your code goes here...
+  end
+end
---
+++ lib/omniauth-trustauth/version.rb
@@ -0,0 +1,5 @@
+module Omniauth
+  module Trustauth
+    VERSION = "0.0.1"
+  end
+end
