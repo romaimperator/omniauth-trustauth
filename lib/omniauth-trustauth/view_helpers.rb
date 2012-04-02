a51dcc8de2ce64fb5fbd37308a673f699ccc432f
romaimperator <romaimperator@gmail.com>
2012-04-02 00:49:05 -0500

    Fixed the trustauth url so that it should work without Devise.

--- lib/omniauth-trustauth/view_helpers.rb
+++ lib/omniauth-trustauth/view_helpers.rb
@@ -2,7 +2,7 @@ module OmniAuth
   module TrustAuth
     module ViewHelpers
       def trustauth_url
-        tag 'meta', :name => 'trustauth', :content => user_omniauth_authorize_path(:trustauth)
+        tag 'meta', :name => 'trustauth', :content => defined?(user_omniauth_authorize_path) ? user_omniauth_authorize_path(:trustauth) : '/auth/trustauth'
       end
     end
   end
